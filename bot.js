6417e1b83c0b7cb72959cdcc1b42aef6579f2988
tigerninjaman <tigerninjaman@gmail.com>
2016-04-03 21:18:05 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = getRandomDeveloperExcuse();
 
   options = {
     hostname: 'api.groupme.com',
@@ -53,5 +53,56 @@ function postMessage() {
   botReq.end(JSON.stringify(body));
 }
 
+//test
+var getHttp = require('../utils').getHttp;
+
+var getRandomDeveloperExcuse = function (cb) {
+  getHttp({
+    hostname: 'developerexcuses.com',
+    path: '/',
+    method: 'GET'
+  }, function onResult(statusCode, data) {
+    // adapted from https://github.com/github/hubot-scripts/blob/master/src/scripts/excuse.coffee#L55
+    var matches = data.match(/<a [^>]+>(.+)<\/a>/i);
+    if (matches && matches[1]) {
+      cb(matches[1]);
+    } else {
+      cb('');
+    }
+  }, function onError() {
+    cb('');
+  });
+};
+
+var getRandomDesignerExcuse = (function () {
+  // Taken from http://designerexcuses.com/js/excuses.js
+  var quotes = [
+    "Oui oui",
+    "k",
+    "GovCo is so far",
+    "linguistics is so cool",
+    "French guys are so hot",
+    "Sorry I have rehearsal",
+    "Sorry I'm at rehearsal",
+    "I had me a boy turned into a man",
+    "SymSys",
+    "Paris",
+    "Baguette",
+    "oui",
+    "Hey, fuck you, buddy",
+    "*wink*",
+    "It's snowing!!",
+    "I have a performance that night",
+    "TAPS stands for 'The Awesomest Part Of Stanford'",
+    "Sops on top!",
+    "Come drink chamomile tea and read checkhov with me",
+    "Anyone else auditioning for the show this quarter?"
+  ];
+
+  return function (cb) {
+    cb(quotes[Math.floor(Math.random() * quotes.length)]);
+  };
+})();
+//end of test
 
 exports.respond = respond;
