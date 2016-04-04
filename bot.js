781d0ecec505abd9079c1987502ff2dad6c4b593
tigerninjaman <tigerninjaman@gmail.com>
2016-04-03 22:42:55 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/ellenbot$/;
+      botRegex = /^ellenbot$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
